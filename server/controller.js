be38264bbeb1ab432cc525e220238c446545432f
Ian Durka <iandurka5503@gmail.com>
2022-09-08 19:02:51 -0700

    update

--- server/controller.js
+++ server/controller.js
@@ -8,6 +8,16 @@ module.exports = {
         let randomCompliment = compliments[randomIndex];
       
         res.status(200).send(randomCompliment);
+    },
+
+        getFortune: (req, res) => {
+            const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
+          
+            // choose random compliment
+            let randomIndex = Math.floor(Math.random() * compliments.length);
+            let randomCompliment = compliments[randomIndex];
+          
+            res.status(200).send(randomCompliment);
     }
 
 }
